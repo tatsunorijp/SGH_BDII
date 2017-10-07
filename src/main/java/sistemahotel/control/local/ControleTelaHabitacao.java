407c9bebdb918cca6856a61c17003aae89e427f1
marceloyb <marcelobenesciutti@gmail.com>
2017-10-07 12:49:04 -0300

    Cadastrar habitacao e salao de festas funcional

--- src/main/java/sistemahotel/control/local/ControleTelaHabitacao.java
+++ src/main/java/sistemahotel/control/local/ControleTelaHabitacao.java
@@ -1,11 +1,15 @@
 package sistemahotel.control.local;
 
+import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTreeTableView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import sistemahotel.control.ControleTelas;
+import sistemahotel.model.local.LocalDAO;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,10 +20,35 @@ import java.util.ResourceBundle;
  */
 public class ControleTelaHabitacao implements Initializable {
     @FXML
+    private JFXTextField tfNumero;
+    @FXML
+    private JFXTextField tfPreco;
+    @FXML
+    private JFXTextField tfCamaSolteiro;
+    @FXML
+    private JFXTextField tfCamaCasal;
+    @FXML
+    private JFXTextField tfInfo;
+    @FXML
     private JFXTreeTableView<?> ttvProdutos;
     @FXML
     AnchorPane apPrincipal;
     ControleTelas window = new ControleTelas();
+    LocalDAO localdao = new LocalDAO();
+
+    public void btNovaHabitacaoActionHandler(ActionEvent e) throws IOException{
+        String numero = tfNumero.getText();
+        String preco = tfPreco.getText();
+        String camasolteiro = tfCamaSolteiro.getText();
+        String camacasal = tfCamaCasal.getText();
+        String informacoes = tfInfo.getText();
+        localdao.NovaHabitacao(numero, camasolteiro, camacasal, preco, informacoes);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Operação realizada com sucesso");
+        alert.setHeaderText(null);
+        alert.setContentText("Habitação Cadastrada");
+        alert.showAndWait();
+    }
 
     public void btVoltarActionHandler(ActionEvent event) throws IOException {
         window.newWindow("/sistemahotel/view/TelaPrincipal.fxml", event);
--- src/main/java/sistemahotel/control/local/ControleTelaSalaoFestas.java
+++ src/main/java/sistemahotel/control/local/ControleTelaSalaoFestas.java
@@ -1,11 +1,16 @@
 package sistemahotel.control.local;
 
+import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTreeTableView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import sistemahotel.control.ControleTelas;
+import sistemahotel.model.local.LocalDAO;
+import sistemahotel.model.local.SalaoFestas;
 
 import java.io.IOException;
 import java.net.URL;
@@ -13,10 +18,33 @@ import java.util.ResourceBundle;
 
 public class ControleTelaSalaoFestas implements Initializable{
     @FXML
+    private JFXTextField tfNumero;
+    @FXML
+    private JFXTextField tfPreco;
+    @FXML
+    private JFXTextField tfMaxPessoas;
+    @FXML
+    private JFXTextField tfInfo;
+    @FXML
     private JFXTreeTableView<?> ttvProdutos;
     @FXML
     AnchorPane apPrincipal;
+
     ControleTelas window = new ControleTelas();
+    LocalDAO localdao = new LocalDAO();
+
+    public void btNovoSalaoActionHandler(ActionEvent e) throws IOException{
+        String numero = tfNumero.getText();
+        String preco = tfPreco.getText();
+        String maxpessoas = tfMaxPessoas.getText();
+        String informacoes = tfInfo.getText();
+        localdao.NovoSalao(numero, maxpessoas, informacoes, preco);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Operação realizada com sucesso");
+        alert.setHeaderText(null);
+        alert.setContentText("Salao de Festas cadastrado");
+        alert.showAndWait();
+    }
 
     public void btVoltarActionHandler(ActionEvent event) throws IOException {
         window.newWindow("/sistemahotel/view/TelaPrincipal.fxml", event);
