e80c903ea27215ac5df711118c3c69c175b8480f
tatsunori <tatsunoripai@gmail.com>
2017-10-08 20:01:59 -0300

    implementado a exibicao dos cadastros de produtos na tabela do menu principal de produtos, servirá de exemplo para os demas gerenciadores para isso foi necessario a criação da classe de retornar listas do BD isso será discutido mais detalhadamente sobre como sera implementado na proxima reuniao

--- src/main/java/sistemahotel/control/produto/ControleTelaProdutos.java
+++ src/main/java/sistemahotel/control/produto/ControleTelaProdutos.java
@@ -1,10 +1,18 @@
 package sistemahotel.control.produto;
 
+import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTreeTableView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import sistemahotel.control.ControleTelas;
+import sistemahotel.model.infraestrutura.Listas;
+import sistemahotel.model.produto.Produto;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,15 +23,50 @@ import java.util.ResourceBundle;
  */
 public class ControleTelaProdutos implements Initializable {
     @FXML
-    private JFXTreeTableView<?> ttvProdutos;
+    TableView tvProdutos;
+    @FXML
+    TableColumn tcNome;
+    @FXML
+    TableColumn tcPreco;
+
+    @FXML
+    private JFXTextField tfNome;
+
+    @FXML
+    private JFXTextField tfPreco;
+
+    @FXML
+    private JFXTextField tfEstoque;
+
+    @FXML
+    private JFXTextField tfAlerta;
+
+    Listas pegaListas;
+    ObservableList list;
     ControleTelas controleTelas = new ControleTelas();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        list = FXCollections.observableList(pegaListas.listProduto());
+        tcNome.setCellValueFactory( new PropertyValueFactory<>("nome"));
+        tcPreco.setCellValueFactory(new PropertyValueFactory<>("preco"));
+        tvProdutos.setItems(FXCollections.observableList(list));
+
+        tvProdutos.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldvalue, newValue) -> selecaoDeItens((Produto) newValue)
+        );
 
     }
 
     public void btNovoProdutoActionHandler(ActionEvent event) throws IOException{
         controleTelas.newWindow("/sistemahotel/view/produto/NovoProduto.fxml",event);
+    }
+
+    public void selecaoDeItens(Produto produto){
+        tfNome.setText(produto.getNome());
+        tfPreco.setText(produto.getPreco());
+        tfEstoque.setText(produto.getQuantidade());
+        tfAlerta.setText(produto.getAlertaEstoque());
+
     }
 }
---
+++ src/main/java/sistemahotel/model/infraestrutura/Listas.java
@@ -0,0 +1,44 @@
+package sistemahotel.model.infraestrutura;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import sistemahotel.model.produto.Produto;
+import sistemahotel.model.infraestrutura.Persistencia;
+
+
+import java.util.List;
+
+/**
+ * Created by tatsunori on 08/10/17.
+ */
+public class Listas {
+    Persistencia persistencia = Persistencia.getInstancia();
+    public static SessionFactory ssf;
+    public static void startSessionFactory() {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        ssf = sessionFactory;
+    }
+
+    public static List<Produto> listProduto() {
+        startSessionFactory();
+        Session session = ssf.openSession();
+        Transaction transaction = null;
+
+        List produto = null;
+
+        try {
+            transaction = session.beginTransaction();
+            produto = session.createQuery("FROM Produto").list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return produto;
+    }
+}
