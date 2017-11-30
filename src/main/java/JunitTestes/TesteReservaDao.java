11ecb6a33015fc439eabb08876b5c662acf58552
megaleuzao <leo_martins@outlook.com>
2017-11-30 19:12:00 -0200

    teste

--- src/main/java/JunitTestes/TesteReservaDao.java
+++ src/main/java/JunitTestes/TesteReservaDao.java
@@ -1,11 +1,12 @@
 package JunitTestes;
 
+import org.hibernate.IdentifierLoadAccess;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.junit.Assert;
 import org.junit.Test;
+import sistemahotel.model.infraestrutura.Persistencia;
 import sistemahotel.model.local.Habitacao;
 import sistemahotel.model.pessoa.Cliente;
 import sistemahotel.model.reserva.Reserva;
@@ -24,22 +25,25 @@ public class TesteReservaDao {
     @Test
     public void novaReserva() throws Exception {
 
-       /* Cliente cliente = new Cliente();
+        Persistencia.getInstancia().startSsf();
+
+        Cliente cliente = new Cliente();
         Habitacao hab = new Habitacao();
         LocalDate in = LocalDate.now();
         LocalDate out = LocalDate.now().plusDays(1);
-        Reserva reserva = new Reserva();
 
-        reservadao.novaReserva(cliente, hab, in, out,"5");
+        Reserva reservahab = reservadao.novaReserva(cliente, hab, in, out,"5");
 
         IdentifierLoadAccess<Reserva> multiLoadAccess = session.byId(Reserva.class);
         Reserva reservahabBd = multiLoadAccess.load(reservahab.getId());
-        Assert.assertNotNull(reservahabBd);*/
+        Assert.assertNotNull(reservahabBd);
 
     }
 
     @Test
     public void checarIndisponibilidade() throws Exception {
+
+        Persistencia.getInstancia().startSsf();
 
         Cliente cliente = new Cliente();
         Habitacao hab = new Habitacao();
@@ -58,18 +62,72 @@ public class TesteReservaDao {
 
     @Test
     public void cancelarReserva() throws Exception {
+
+        Cliente cliente = new Cliente();
+        Habitacao hab = new Habitacao();
+        LocalDate in = LocalDate.now();
+        LocalDate out = LocalDate.now().plusDays(1);
+
+        Reserva reservahab = reservadao.novaReserva(cliente, hab, in, out,"5");
+        reservadao.cancelarReserva(reservahab);
+
+        IdentifierLoadAccess<Reserva> multiLoadAccess = session.byId(Reserva.class);
+        Reserva reservahabBd = multiLoadAccess.load(reservahab.getId());
+        assertNull(reservahabBd);
+
     }
 
     @Test
     public void fazerCheckIn() throws Exception {
+
+        Cliente cliente = new Cliente();
+        Habitacao hab = new Habitacao();
+        LocalDate in = LocalDate.now();
+        LocalDate out = LocalDate.now().plusDays(1);
+
+        Reserva reservahab = reservadao.novaReserva(cliente, hab, in, out,"5");
+        reservadao.fazerCheckIn(reservahab);
+
+        IdentifierLoadAccess<Reserva> multiLoadAccess = session.byId(Reserva.class);
+        Reserva reservahabBd = multiLoadAccess.load(reservahab.getId());
+        assertEquals("Em andamento", reservahabBd.getStatus());
+
     }
 
     @Test
     public void fazerCheckOut() throws Exception {
+
+        Cliente cliente = new Cliente();
+        Habitacao hab = new Habitacao();
+        LocalDate in = LocalDate.now();
+        LocalDate out = LocalDate.now().plusDays(1);
+
+        Reserva reservahab = reservadao.novaReserva(cliente, hab, in, out,"5");
+        reservadao.fazerCheckOut(reservahab);
+
+        IdentifierLoadAccess<Reserva> multiLoadAccess = session.byId(Reserva.class);
+        Reserva reservahabBd = multiLoadAccess.load(reservahab.getId());
+        assertEquals("Finalizada", reservahabBd.getStatus());
+
     }
 
     @Test
     public void estenderReserva() throws Exception {
+
+        Cliente cliente = new Cliente();
+        Habitacao hab = new Habitacao();
+        LocalDate in = LocalDate.now();
+        LocalDate out = LocalDate.now().plusDays(1);
+
+        Reserva reservahab = reservadao.novaReserva(cliente, hab, in, out,"5");
+        reservadao.estenderReserva(reservahab, in.plusDays(5), out.plusDays(5));
+
+        IdentifierLoadAccess<Reserva> multiLoadAccess = session.byId(Reserva.class);
+        Reserva reservahabBd = multiLoadAccess.load(reservahab.getId());
+
+        assertEquals(reservahabBd.getDataCheckIn(), in.plusDays(5));
+        assertEquals(reservahabBd.getDataCheckOut(), out.plusDays(5));
+
     }
 
 
